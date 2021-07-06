c4b57d6b07bf4b527f5aeb316d8975086c6ebbad
zingimmick <zingimmick@outlook.com>
2021-07-06 15:06:41 +0800

    Fix cs

--- rector.php
+++ rector.php
@@ -47,11 +47,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     );
     $parameters->set(
         Option::PATHS,
-        [
-            __DIR__ . '/src',
-            __DIR__ . '/tests',
-            __DIR__ . '/ecs.php',
-            __DIR__ . '/rector.php',
-        ]
+        [__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']
     );
 };
