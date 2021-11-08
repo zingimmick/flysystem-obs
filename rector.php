509691ebddb9ef7ad4d488a3ef34780fc2745958
zingimmick <zingimmick@outlook.com>
2021-11-08 18:38:38 +0800

    Enable TypedPropertyFromStrictGetterMethodReturnTypeRector

--- rector.php
+++ rector.php
@@ -11,7 +11,6 @@ use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
 use Rector\Privatization\Rector\Class_\RepeatedLiteralToClassConstantRector;
 use Rector\Privatization\Rector\MethodCall\PrivatizeLocalGetterToPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
-use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictGetterMethodReturnTypeRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Zing\CodingStandard\Set\RectorSetList;
 
@@ -30,7 +29,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
             FinalizeClassesWithoutChildrenRector::class,
             RepeatedLiteralToClassConstantRector::class,
             PrivatizeLocalGetterToPropertyRector::class,
-            TypedPropertyFromStrictGetterMethodReturnTypeRector::class,
         ]
     );
     $parameters->set(
