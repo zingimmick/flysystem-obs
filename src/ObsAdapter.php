c8db72a130a9990670319660b3a6e8ff7c5b1185
zingimmick <zingimmick@outlook.com>
2022-01-17 21:30:36 +0800

    Fix style

--- src/ObsAdapter.php
+++ src/ObsAdapter.php
@@ -182,6 +182,7 @@ class ObsAdapter implements FilesystemAdapter
     public function copy(string $source, string $destination, Config $config): void
     {
         try {
+            /** @var string $visibility */
             $visibility = $this->visibility($source)
                 ->visibility();
         } catch (Throwable $exception) {
