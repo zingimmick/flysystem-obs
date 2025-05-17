7d0360735f68284dbf8616a3b396a232c8594d8d
zingimmick <zingimmick@outlook.com>
2025-05-17 12:44:16 +0800

    wip

--- tests/MockAdapterTest.php
+++ tests/MockAdapterTest.php
@@ -1016,7 +1016,6 @@ final class MockAdapterTest extends TestCase
             ]));
         $this->mockGetMetadata('a/b/file.txt');
         $contents = iterator_to_array($this->obsAdapter->listContents('a', true));
-        $this->assertContainsOnlyInstancesOf(StorageAttributes::class, $contents);
         $this->assertCount(2, $contents);
 
         /** @var \League\Flysystem\FileAttributes $file */
--- tests/ValidAdapterTest.php
+++ tests/ValidAdapterTest.php
@@ -222,7 +222,6 @@ class ValidAdapterTest extends TestCase
 
         /** @var \League\Flysystem\StorageAttributes[] $contents */
         $contents = iterator_to_array($this->obsAdapter->listContents('fixture/path', true));
-        $this->assertContainsOnlyInstancesOf(StorageAttributes::class, $contents);
         $this->assertCount(2, $contents);
 
         /** @var \League\Flysystem\FileAttributes $file */
